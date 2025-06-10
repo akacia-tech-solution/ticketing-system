bd11eba8c3faf5fd47f75572dde101d66a6be3eb
APPLE <apple@APPLEs-MacBook-Air.local>
2025-06-10 20:57:22 +0100

    updated logo

--- enterprise/app/models/captain/assistant.rb
+++ enterprise/app/models/captain/assistant.rb
@@ -68,6 +68,6 @@ class Captain::Assistant < ApplicationRecord
   private
 
   def default_avatar_url
-    "#{ENV.fetch('FRONTEND_URL', nil)}/assets/images/dashboard/captain/logo.svg"
+    "#{ENV.fetch('FRONTEND_URL', nil)}/assets/images/dashboard/captain/logo.png"
   end
 end
